af390d3465f63a3cc60e3676ea5aa8a62f47850d
Eu4ng <eu4ng97@gmail.com>
2023-12-23 09:55:26 +0900

    fix : 폴더 구조 변경 및 어드레서블 에셋 교체

---
+++ ContentsDownloadTest/Assets/ContentDownloadTest/Scripts/AddressablesTest.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressablesTest : MonoBehaviour
+{
+    [SerializeField] AssetReferenceGameObject m_Target;
+
+    void Start()
+    {
+        // 어드레서블 에셋 스폰
+        var target = Addressables.InstantiateAsync(m_Target);
+    }
+}
